1328ed78a8c6733b4c5d2c144e50bd954f7da873
Michael <AstroMike101@gmail.com>
2021-08-01 16:02:22 -0700

    cleaned up js code a bit and made background image fit better

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 let btnSound = document.querySelector("#bubble-sound");
-let musicSound = document.querySelector("#music-sound");
-let musicBtn = document.querySelector('.musicBtn'); 
+let musicSound = document.querySelector("#music-sound"); // the music thats playing
+let musicBtn = document.querySelector('.musicBtn');  //the music button 
 let start = document.querySelector(".start-btn");
 let body = document.querySelector(".app");
 let headerDiv = document.querySelector('.headerDiv');
@@ -10,6 +10,7 @@ let levelsBtn = document.querySelector('.levelsBtn');
 let levelsPage = document.querySelector('#levels-page');
 let gameDiv = document.querySelector('#game');
 
+//function to begin the music 
 function musicStart(){ 
     musicSound.play();  
 }
@@ -42,55 +43,63 @@ function main() {
 
   let isPlaying = true;
   let header = document.createElement("div"); //  quiz question
+  header.classList.add("quiz-scr") 
+  body.appendChild(header);
   let judge = document.createElement("h2"); //says right or wrong under quiz 
+  judge.classList.add("judge")
+  judge.textContent = "Choose the correct option"; 
+
+  body.appendChild(judge);
   let options = Array.from({ length: 3 }, () =>
-    document.createElement("button")
+    document.createElement("button") //creates 3 buttons
     
   );  
-  //initialize new music button at bottom left
-  let musicBtnMain = document.createElement("button")
-
-//scoreboard creation, adding a class to it, and adding text
-  let scoreBoard = document.createElement('p')
-  scoreBoard.classList.add('scoreBoard')
-
-  // scoreBoard.textContent = `Score: ${score}`;
-
- //creates a div for the choice buttons 
+  //creates a div for the choice buttons 
   let choicesDiv = document.createElement('div');
   choicesDiv.classList.add('choicesDiv');
-
-
-  let nextBtnDiv = document.createElement('div');
-  nextBtnDiv.classList.add('nextBtnDiv')
-  
-
-  let nextbtn = document.createElement("button")  
-  body.appendChild(header);
-  headerDiv.appendChild(scoreBoard);
-  body.appendChild(judge);
   body.appendChild(choicesDiv);
-  body.appendChild(nextBtnDiv);
   choicesDiv.appendChild(options[0]);
   choicesDiv.appendChild(options[1]);
   choicesDiv.appendChild(options[2]);
+
+  //initialize new music button at bottom left
+  let musicBtnMain = document.createElement("button")
+  musicBtnMain.classList.add("musicBtnmain")
+  choicesDiv.appendChild(musicBtnMain);
+  musicBtnMain.innerHTML = "<img class=\"music-btn-pic\" src=\"../images/sound.png\"></img>"  // music button image
+  musicBtn.remove(); // removes the music button from homescreen
+
   
-  judge.classList.add("judge")
-  header.classList.add("quiz-scr") 
+
+//scoreboard creation, adding a class to it, 
+  let scoreBoard = document.createElement('p')
+  scoreBoard.classList.add('scoreBoard')
+  headerDiv.appendChild(scoreBoard); 
+  //adds the scoreboard to the headerDiv, which is the div that houses levelSelect and score
+
+  // scoreBoard.textContent = `Score: ${score}`;
+
+
+//creates a Div that houses the Next button
+  let nextBtnDiv = document.createElement('div');
+  nextBtnDiv.classList.add('nextBtnDiv')
+  body.appendChild(nextBtnDiv);
+  
+
+  let nextbtn = document.createElement("button")  //creates the actual next button 
+  nextbtn.classList.add("nextBtn")
+  
+
 
   judge.textContent = "Choose the correct option:";
-  
+  //adds the choices class to all three option buttons
   options[0].classList.add("choices")
   options[1].classList.add("choices")
   options[2].classList.add("choices")
-  nextbtn.classList.add("nextBtn")
+ 
   
-  //appending the main musicBtn
-  choicesDiv.appendChild(musicBtnMain)
-  musicBtnMain.innerHTML = "<img class=\"music-btn-pic\" src=\"../images/sound.png\"></img>"
-  nextBtnDiv.appendChild(musicBtnMain)
-  musicBtnMain.innerHTML = "<img class=\"music-btn-pic\" src=\"./images/sound.png\"></img>"
-  musicBtnMain.classList.add("musicBtnmain")
+  
+// if music is playing and button is clicked, stop it, and likewise if not playing
   
   musicBtnMain.addEventListener("click", () => {
     if(isPlaying){
@@ -102,16 +111,17 @@ function main() {
       isPlaying = true;
     } 
   })
-  //removing old button
-  musicBtn.remove();
-  let score = 0
-  scoreBoard.textContent = `Score: ${score}`;
-  // scoreBoard.textContent = `Score: ${score}`;
 
-  random();
+
+
+//initialize score and sets the scoreboard to score
+  let score = 0;
+  scoreBoard.textContent = `Score: ${score}`;
+
   
   function random() {
-    let random = Math.ceil(Math.random() * quiz.length - 1);
+    let random = Math.ceil(Math.random() * quiz.length -1);
+
       choices(
         quiz[random].question,
         quiz[random].answer,
@@ -121,9 +131,10 @@ function main() {
         quiz[random].correct,
         quiz[random].options
         );
-        judge.textContent = "Choose the correct option"; 
-      nextbtn.remove() 
+        nextbtn.remove() 
     }
+
+    random();
   
   function next(){
     nextBtnDiv.appendChild(nextbtn);
@@ -131,10 +142,12 @@ function main() {
     judge.textContent = "Well done!"
     nextbtn.addEventListener("click", () => random());
   }
+
+
   
   function choices(question, answer, option1, option2, option3, correct, op){
-
-    header.innerHTML = question;  
+    
+    header.textContent = question;  
 
     let scoreText = document.querySelector('.scoreBoard');
 
@@ -155,7 +168,6 @@ function main() {
     header.innerHTML = correct;
     score += 100;
     scoreText.innerHTML = `Score: ${score}`;
-
     setTimeout(() => {next()}, 500);
     }
 
@@ -163,20 +175,25 @@ function main() {
     judge.textContent = "Wrong"; 
     }
 
+
+
     function answerCheck(option) {
+      
       console.log(op);
       console.log(op[answer]);
 
-      if (option.innerHTML === op[answer]) {
+      if (option.textContent === op[answer]) {
         option.classList.add('greenChange');
         removeGreenChange(option);
         displayWin();
+        
       }
       else {
         option.classList.add("redChange");
         removeRedChange(option);
         displayLoss();
       }
+      
       // cleanOpArray();
     }
 
